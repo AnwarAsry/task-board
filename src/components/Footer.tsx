9956d5a94709834f1d7b25bfcd1f57e26bb42116
AnwarAsry <anwareducation03@gmail.com>
2026-09-01 11:09:43 +0200

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,6 @@
+import styles from '../css/footer.module.css'
+export const Footer = () => {
+    return <footer className={styles.footer}>
+        <p>@2026 All rights reserved.</p>
+    </footer>
+}

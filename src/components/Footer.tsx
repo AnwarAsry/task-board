8794b6fb3c3fd6588f64782f66ad0e042564784c
AnwarAsry <anwareducation03@gmail.com>
2026-09-04 19:27:53 +0200

    styled footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,5 @@
-import styles from '../css/footer.module.css'
 export const Footer = () => {
-    return <footer className={styles.footer}>
+    return <footer className="py-6 text-sm text-center text-gray-200 bg-indigo-950">
         <p>@2026 All rights reserved.</p>
     </footer>
 }
